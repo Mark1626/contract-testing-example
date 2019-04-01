7438503cc4fc2a5a6008f9f5fd3743fabc02e7b9
m4rk9696 <nimalan.m@protonmail.com>
2019-04-01 18:39:28 +0530

    feat: Add producer and consumer

---
+++ consumer/src/main/java/com/mark/Application.java
@@ -0,0 +1,14 @@
+package com.mark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
